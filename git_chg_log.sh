52fd46ab642a2c9f21dfc6b253f33439c863713d
suzhen <suzhen@homeinpocket.net>
2019-11-14 15:09:12 +0800

    :recycle: 抽象 tag_sha1_id 函数

--- git_chg_log.sh
+++ git_chg_log.sh
@@ -10,14 +10,16 @@ tag_arr=($(git tag -l --sort=committerdate | tac | tr "\n" " "))
 
 tag_arr_len=${#tag_arr[@]}
 
-head_tag_id=$(git show -s HEAD --format="%h")
-
-function latest_tag_id() {
-  git show -s "${tag_arr[0]}" --format="%h"
+function tag_sha1_id() {
+  if [ -z "${1}" ]; then
+    echo "name of commit is required"
+    exit 1
+  fi
+  git show -s "${1}" --format="%h"
 }
 
-if [ "${tag_arr_len}" == "0" ] || [ "${head_tag_id}" == "$(latest_tag_id)" ]; then
-  git log -n${lastCountIfNoTag} --pretty=format:"${logFormat}" --date=format:"${dateFormat}"  | cat
+if [ "${tag_arr_len}" == "0" ] || [ "$(tag_sha1_id "HEAD")" == "$(latest_tag_id "${tag_arr[0]}")" ]; then
+  git log -n${lastCountIfNoTag} --pretty=format:"${logFormat}" --date=format:"${dateFormat}" | cat
 else
-  git log --pretty=format:"${logFormat}" --date=format:"${dateFormat}" ${tag_arr[0]}..HEAD | cat
+  git log --pretty=format:"${logFormat}" --date=format:"${dateFormat}" "${tag_arr[0]}"..HEAD | cat
 fi
